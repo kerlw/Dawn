fe6d860ca05c5c35f430f867ac2763818f207724
Shirish Agarwal <shirishag75@gmail.com>
2012-04-26 23:52:09 +0530

    no wonder they call me a polisher

---
+++ data/zone1.shadow.lua
@@ -0,0 +1,3 @@
+EditorInterface.addEnvironment( -1941, -2352, 0, MapData.DontSave.shadow_shadow1_tga );
+EditorInterface.addEnvironment( -1941, -2352, 0, MapData.DontSave.shadow_shadow1_tga );
+EditorInterface.addEnvironment( -1941, -2352, 0, MapData.DontSave.shadow_shadow1_tga );
